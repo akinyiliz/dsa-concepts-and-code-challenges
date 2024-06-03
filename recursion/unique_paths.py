31e4e6020fb430f64f5ad32e3c583962f705bf88
akinyiliz <lizzieke84@gmail.com>
2024-06-03 08:55:30 +0300

    refactor: Improve Efficiency by using memoization

--- recursion/unique_paths.py
+++ recursion/unique_paths.py
@@ -6,11 +6,25 @@ paths from the upper-leftmost square to the lower-rightmost square.
 '''
 
 
-def unique_paths(rows, columns):
+# def unique_paths(rows, columns):
+#     if rows == 1 or columns == 1:
+#         return 1
+
+#     return unique_paths(rows - 1, columns) + unique_paths(rows, columns - 1)
+
+
+''' Improve efficiency using Memoization '''
+
+
+def unique_paths(rows, columns, memo={}):
     if rows == 1 or columns == 1:
         return 1
 
-    return unique_paths(rows - 1, columns) + unique_paths(rows, columns - 1)
+    if not memo.get("rc"):
+        memo["rc"] = unique_paths(
+            rows - 1, columns, memo) + unique_paths(rows, columns - 1, memo)
+
+    return memo["rc"]
 
 
 # Example Usage
